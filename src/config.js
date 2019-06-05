6fb16cec30834a8fdef1fb31e44dcde97fb634c4
Farah Student <farah.gus@hotmail.com>
2019-06-04 22:35:13 -0400

    log

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   DB_URL: process.env.DB_URL
-    || 'postgresql://dunder-mifflin@localhost/spaced-repetition',
+    || 'postgres://cgddcqawdkkmeb:2215df5a835a46916425e38e001c3bb091164d41728298da602cfd558ee693f2@ec2-107-20-155-148.compute-1.amazonaws.com:5432/dfjj3p2cg4ri47',
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
   JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
 }
--- src/user/user-router.js
+++ src/user/user-router.js
@@ -53,6 +53,7 @@ userRouter
         .location(path.posix.join(req.originalUrl, `/${user.id}`))
         .json(UserService.serializeUser(user))
     } catch(error) {
+      console.log(error)
       next(error)
     }
   })
